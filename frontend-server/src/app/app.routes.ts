79e199153e9969b7ebd4b621bbba1c8ac953fad5
Constanza Venegas <constanza.venegas2201@alumnos.ubiobio.cl>
2026-05-25 09:09:47 -0400

    feat: implementar loginGuard

--- frontend-server/src/app/app.routes.ts
+++ frontend-server/src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { ProductComponent } from './features/products/components/product/product
 import { Welcome } from './features/home/welcome/welcome';
 import { PageNotFound } from './features/not-found/page-not-found/page-not-found';
 import { Login } from './features/auth/components/login/login';
-import { authGuard, noAuthGuard } from './core/guards/auth.guard';
+import { loginGuard } from './features/auth/guards/login-guard';
 
 export const routes: Routes = [
-  { path: 'home',     component: Welcome,          canActivate: [authGuard] },
-  { path: 'products', component: ProductComponent,  canActivate: [authGuard] },
-  { path: 'login',    component: Login,             canActivate: [noAuthGuard] },
+  { path: 'home',     component: Welcome,          canActivate: [loginGuard] },
+  { path: 'products', component: ProductComponent,  canActivate: [loginGuard] },
+  { path: 'login',    component: Login },
   { path: '',         redirectTo: 'login',          pathMatch: 'full' },
   { path: '**',       component: PageNotFound }
 ];
